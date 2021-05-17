6b57212ad89b26d1d557d64860232e77a8bcb80d
Anil Limbu <tshonganilimbu71@gmail.com>
2021-05-17 21:48:50 +1000

    Update SQLQuery2.sql

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -14,7 +14,7 @@ order by 3,4
 --Selecting Data from CovidDeaths table
 Select location, date, total_cases, new_cases, total_deaths, population
 From PortfolioProject..CovidDeaths
-where continent is not null           -- Removes the continents leaving only countries
+where continent is not null           -- Removes continent
 order by 1,2
 
 
